18898aa82cfc81f5fe51ee36f9c274f3d0fb5c31
Md Khalilul Rahman <105421454+Ashif2409@users.noreply.github.com>
2024-02-18 14:48:02 +0530

    Update Reg.js

--- frontend/src/Components/Reg.js
+++ frontend/src/Components/Reg.js
@@ -22,7 +22,7 @@ export const Reg = () => {
         const { email, name, password, confirmPass } = user;
         if (email && name && password && confirmPass) {
             if (password === confirmPass) {
-                axios.post('http://localhost:8080/register', user)
+                axios.post('book-recommendation-woad.vercel.app/register', user)
                     .then(res => {
                         navigate('/');
                         console.log('Successfully Register');
